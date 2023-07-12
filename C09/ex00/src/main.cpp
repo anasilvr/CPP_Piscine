fddd3bf66650127a8326d01ed91f63bf32efe2f5
Ana Luiza Rodrigues Silveira <anarodri@c2r5p3.42quebec.com>
2023-07-12 14:56:55 -0400

    Update TODO list

--- C09/ex00/src/main.cpp
+++ C09/ex00/src/main.cpp
@@ -1,29 +1,29 @@
 /*
 TODO:
 
-[ ] Create a program which outputs the value of a certain amount of bitcoin on
+[x] Create a program which outputs the value of a certain amount of bitcoin on
 a certain date.
-[ ] Use a database in csv format which will represent bitcoin
+[x] Use a database in csv format which will represent bitcoin
 price over time.
-[ ] The program will take as input a second database, storing
+[x] The program will take as input a second database, storing
 the different prices/dates to evaluate.
 
 Your program must respect these rules:
     [x] The program name is btc.
-    [ ] Your program must take a file as argument.
-        [ ] Each line in this file must use the following format: "date | value"
-		[ ] A valid date will always be in the following format: Year-Month-Day [Error: bad input => 2001-42-42]
-        [ ] A valid value must be either a float or a positive integer
+    [x] Your program must take a file as argument.
+        [x] Each line in this file must use the following format: "date | value"
+		[x] A valid date will always be in the following format: Year-Month-Day [Error: bad input => 2001-42-42]
+        [x] A valid value must be either a float or a positive integer
 		between 0 and 1000. [Error: not a positive number.] // [Error: too large a number.]
 
-[ ] STDOUT: the result of the value multiplied by the exchange rate according
-to the date indicated in your database. [ ] If the date used in the input does
-not exist in your DB then you must use the closest date contained in your DB [
-] Be careful to use the lower date and not the upper one.
+[x] STDOUT: the result of the value multiplied by the exchange rate according
+to the date indicated in your database. [x] If the date used in the input does
+not exist in your DB then you must use the closest date contained in your DB
+[x] Be careful to use the lower date and not the upper one.
 
-[ ] You must use at least one container in your code to validate this (map? pair?)
-[ ] You should handle possible errors with an appropriate error message.
-[ ] Validate file permissions
+[x] You must use at least one container in your code to validate this (map)
+[x] You should handle possible errors with an appropriate error message.
+[x] Validate file permissions
 */
 
 #include <string>
